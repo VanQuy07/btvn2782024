ce781e6b434b34ac0584304fb9afd63abcba8b9c
Quý <150776876+VanQuy07@users.noreply.github.com>
2024-08-27 21:57:14 +0700

    Maxmin.java

--- Maxmin.java
+++ Maxmin.java
@@ -1,20 +1,32 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package maxmin;
 
-/**
- *
- * @author LENOVO
- */
+import java.util.Scanner;
+
 public class Maxmin {
 
-    /**
-     * @param args the command line arguments
-     */
+  
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhap so luong phan tu cua mang: ");
+        int n = scanner.nextInt();
+        int[] array = new int[n];
+        for (int i = 0; i < n; i++) {
+            System.out.print("Phan tu thu " + (i + 1) + ": ");
+            array[i] = scanner.nextInt();
+        }
+        int max = array[0];
+        int min = array[0];
+           for (int i = 1; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        System.out.println("Gia tri lon nhat trong mang: " + max);
+        System.out.println("Gia tri nho nhat trong mang: " + min);
     }
     
 }
